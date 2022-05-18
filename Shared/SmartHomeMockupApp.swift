4f0e343ffa7f075ca5f74e3d5c33d93d267217f2
40314471 <cclarke60@qub.ac.uk>
2022-05-18 07:22:28 +0100

    Initial Commit

---
+++ Shared/SmartHomeMockupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeMockupApp.swift
+//  Shared
+//
+//  Created by Charles Richard Clarke on 18/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeMockupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
